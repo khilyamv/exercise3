62b6f31b4df38045b3d25cceb25d6617aaeaea29
khilyamv <47350562+khilyamv@users.noreply.github.com>
2019-04-01 15:53:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var request = new XMLHttpRequest();
-var data;
+var data, getData
 var tabel = document.querySelector("#tabel");
 var cari = document.querySelector("#cari");
-var pesan = document.querySelector("#pesan");
+//var pesan = document.querySelector("#pesan");
 
 function hide(x){
     document.querySelector(x).style.visibility = 'hidden';
@@ -12,7 +12,7 @@ function show(x){
     document.querySelector(x).style.visibility = 'visible';
 }
 
-hide("#pesan");
+//hide("#pesan");
 
 request.open('GET', 'https://swapi.co/api/planets/?format=json', true);
 request.onload = function() {
@@ -24,11 +24,13 @@ request.onload = function() {
         for (var i=0; i<data.length; i++){        
             const row = tabel.insertRow(-1);
             const name = row.insertCell(-1);
+            const rotation = row.insertCell(-1);
             const climate = row.insertCell(-1);
-            const gravity = row.insertCell(-1);
+            const terrain = row.insertCell(-1);
             name.textContent = data[i].name;
+            rotation.textContent = data[i].rotation_period;
             climate.textContent = data[i].climate;
-            gravity.textContent = data[i].gravity;
+            terrain.textContent = data[i].terrain;
         }
     }
     
@@ -39,34 +41,64 @@ request.onload = function() {
             }
     }
     
-    function ketemu(){
-        var getData = data.filter(key => key.name === cari.value);
-        if (cari.value == "") {
-            hide("#pesan");
-            hapus();
-            tampil();
-        } else if (getData.length>0) {
-            hapus();
-            hide("#pesan");
+    function carikan(){
+        if (getData.length>0) {
+//            hide("#pesan");
             for (var k=0; k<getData.length;k++){        
                 const row = tabel.insertRow(-1);
                 const name = row.insertCell(-1);
+                const rotation = row.insertCell(-1);
                 const climate = row.insertCell(-1);
-                const gravity = row.insertCell(-1);
+                const terrain = row.insertCell(-1);
                 name.textContent = getData[k].name;
+                rotation.textContent = getData[k].rotation_period;
                 climate.textContent = getData[k].climate;
-                gravity.textContent = getData[k].gravity;
+                terrain.textContent = getData[k].terrain;
             }
-        } else {
-            show("#pesan");
-            pesan.innerHTML = "<i class='large frown outline icon'></i>Oops, planet <b>" + cari.value + "</b> not found!";
         }
+    }
+    
+    function cariNama(){
+        getData = data.filter(key => key.name.includes(cari.value));
+        carikan();
+    }
+    
+    function cariRotation(){
+        getData = data.filter(key => key.rotation_period.includes(cari.value));
+        carikan();
+    }
+    
+    function cariClimate(){
+        getData = data.filter(key => key.climate.includes(cari.value));
+        carikan();
+    }
+    
+    function cariTerrain(){
+        getData = data.filter(key => key.terrain.includes(cari.value));
+        carikan();
     }
     
     cari.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
             event.preventDefault();
-            ketemu();
+            hapus();
+            cariNama();
+            cariRotation();
+            cariClimate();
+            cariTerrain();
+            if (cari.value == "") {
+//                hide("#pesan");
+                hapus();
+                tampil();
+            } else if (tabel.rows.length == 1) {
+//                show("#pesan");
+//                pesan.innerHTML = "<i class='large frown outline icon'></i>Oops, data <b>" + cari.value + "</b> not found!";
+                row = tabel.insertRow(-1);
+                const message = row.insertCell(-1);
+                message.colSpan = 4;
+                message.innerHTML = '<div class="ui negative icon message"><i class="large frown outline icon"></i>Oops, data&nbsp;<b>' + cari.value + '</b>&nbsp;not found!</div>';
+//                message.innerHTML = '<p class=" ui negative message"><i class="large frown outline icon></i>Oops, data <b>' + cari.value + '</b> not found!</p>';
+            }
         }
     });
 };
